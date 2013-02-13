d5d48f4566c67e47db59070423386b5402a427ff
Josh Grant <joshua.m.grant@gmail.com>
2013-02-12 21:27:35 -0500

    initial commit of some base classes

---
+++ tests/BasePage.py
@@ -0,0 +1,11 @@
+
+from abstractPage import abstractPage
+
+class BasePage(abstractPage):
+    """
+    Base class for each page
+    """
+
+    def init(self):
+        pass
+        
---
+++ tests/abstractPage.py
@@ -0,0 +1,7 @@
+
+class abstractPage():
+    """
+    Abstract page object for use with testing.
+    Contains webdriver initialization and general utility methods
+    """
+    pass
