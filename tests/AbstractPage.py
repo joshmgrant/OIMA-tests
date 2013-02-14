2becf47f0b09d1af3a0285ec6f5b1e2afaab27e1
Josh Grant <joshua.m.grant@gmail.com>
2013-02-13 21:10:15 -0500

    added __init__ and other methods to AbstractPage

--- tests/AbstractPage.py
+++ tests/AbstractPage.py
@@ -1,7 +1,13 @@
+from selenium import webdriver
 
-class abstractPage():
+class AbstractPage():
     """
     Abstract page object for use with testing.
     Contains webdriver initialization and general utility methods
     """
-    pass
+    
+    def __init__(self, driver):
+        self.driver = driver
+
+    def getExpectedPageName():
+        raise NotImplementedError("getExpectedPageName() not implemented")
