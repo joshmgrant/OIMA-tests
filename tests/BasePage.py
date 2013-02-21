ce277e3c3156a7c28437c2e765a22c399bd8d3bf
Josh Grant <joshua.m.grant@gmail.com>
2013-02-20 21:14:27 -0500

    moved some code from AbstractPage to BasePage

--- tests/BasePage.py
+++ tests/BasePage.py
@@ -1,11 +1,14 @@
 
 from AbstractPage import AbstractPage
+from selenium import webdriver
 
 class BasePage(AbstractPage):
     """
     Base class for each page
     """
 
-    def init(self):
-        pass
-        
+    def __init__(self, driver):
+        self.driver = driver
+
+    def getExpectedPageName():
+        raise NotImplementedError("getExpectedPageName() not implemented")
