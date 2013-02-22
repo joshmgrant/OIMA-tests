e0350ee4c30272db11f748c2dc479b1abc3cc095
Josh Grant <joshua.m.grant@gmail.com>
2013-02-21 22:36:58 -0500

    more updates to some base classes

--- tests/BaseTest.py
+++ tests/BaseTest.py
@@ -1,8 +1,23 @@
-import nose
+
 import unittest
+from selenium import webdriver
 
 class BaseTest(unittest.TestCase):
     """
     Base test class. All other test classes derive from this one
     """
-    
+
+    @classmethod
+    def startBrowser(self):
+        self.driver = webdriver.Firefox()
+        self.landingPage = 'http://oima-aimo.ca'
+
+    def setUp(self):
+        self.driver.get(landingPage)
+
+    @classmethod
+    def closeBrowser(self):
+        self.driver.quit()
+
+    def aTest(self):
+        print 'run this test'
