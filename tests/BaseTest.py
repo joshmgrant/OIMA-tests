22c0af083732fd008210b4892048255f449302a2
Josh Grant <joshua.m.grant@gmail.com>
2013-02-25 21:16:58 -0500

    added classmethod decorator to BaseTest, seems to work

--- tests/BaseTest.py
+++ tests/BaseTest.py
@@ -7,25 +7,28 @@ class BaseTest(unittest.TestCase):
     Base test class. All other test classes derive from this one
     """
 
-    def __init__(self, browser):
-        self.browser = browser
-        self.landingPage = 'http://oima-aimo.ca'
-
     @classmethod
     def setupClass(self):
-        self.driver = self.setupWebDriver()
+        self.browser = 'firefox'
+        self.landingPage = 'http://oima-aimo.ca'
+        
+        # should be abstracted to its own method, setupWebDriver
+        if self.browser.lower() == 'firefox':
+            self.driver = webdriver.Firefox()
+        elif self.browser.lower() == 'chrome':
+            self.driver =  webdriver.Chrome()
+        else:
+            raise RuntimeError("Invalid browser selection, try again")
+
+        self.loadLandingPage()
 
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
 
+    @classmethod
     def loadLandingPage(self):
         self.driver.get(self.landingPage)
     
     def setupWebDriver(self):
-        if browser.lower() == 'firefox':
-            return webdriver.Firefox()
-        elif browser.lower() == 'chrome':
-            return webdriver.Chrome()
-        else:
-            raise RuntimeError("Invalid browser selection, try again")
+        pass
